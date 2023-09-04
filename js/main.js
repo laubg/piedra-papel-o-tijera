f4f09ed8b5d2a74f4e97ce92648bbdf25321f277
laubg <laurabautistg@gmail.com>
2023-09-04 05:35:34 -0300

    estaría listo el contador de jugadas

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ let jugadasGanadasPc= 0;
 let rondasEmpatadas= 0;
 
 
+// const eleccUsuario= document.getElementById("resultadoUsuario")
+// const eleccPc= document.getElementById("resultadoPc")
+// const resRonda= document.getElementById("resultadoRonda")
+
 // <--------Pedir nombre a usuario y darle la bienvenida----->
 let nombre="";
 const input = document.querySelector("input");
@@ -51,7 +55,7 @@ const comenzar = document.getElementById("comenzar");
 
 function crearJuego (){
     // <--------Ocultar botón de Inicio de jugada----->
-    const botonInicio= document.getElementById("IniciarJuego")
+    botonInicio= document.getElementById("IniciarJuego");
     botonInicio.style.display = 'none';
     // <--------Imprimir botones para que usuario elija ----->
     comenzar.innerHTML = `<h3> Elije una opción:</h3><br>
@@ -93,10 +97,10 @@ function crearJuego (){
 
 function obtenerJugadaUsuario(){
     // <--------Determinar elección de usuario----->
-    let opcion = document.querySelector('input[name="eleccion"]:checked')
+    let opcion = document.querySelector('input[name="eleccion"]:checked');
     seleccionUs= opcion.value;
 
-    const eleccUsuario= document.getElementById("resultadoUsuario")
+   const eleccUsuario= document.getElementById("resultadoUsuario");
 
     // <--------Imprimir elección de usuario----->
 
@@ -160,7 +164,7 @@ function obtenerJugadaPC() {
 
 // <---------Paso 3:Comparar ambos valores y determinar el ganador de cada ronda------->
 function detGanadorRonda(){
-    const resRonda= document.getElementById("resultadoRonda")
+    const resRonda= document.getElementById("resultadoRonda");
     // <--------Imprimir ganador de la ronda----->
     if ((seleccionUs==0 & seleccionPc==0)||(seleccionUs==1 & seleccionPc==1)||(seleccionUs==2 & seleccionPc==2)){
         resRonda.innerHTML = `
@@ -191,7 +195,15 @@ function detGanadorRonda(){
 
 function determinarGanador(){
     // numeroRondas
-    if (numeroRondas==5){
+    
+
+    if (numeroRondas>5){
+        // <--------Ocultar los resultados de las jugadas----->
+        // resRonda.style.display = 'none';
+        // eleccPc.style.display = 'none';
+        // eleccUsuario.style.display = 'none';
+        // comenzar.style.display = 'none';
+
         // <--------Mostrar el resultado de la jugada con sus estilos----->
         const resultJuego= document.getElementById("resultadoJuego")
         resultJuego.style.display = 'block';
@@ -201,19 +213,19 @@ function determinarGanador(){
         if (jugadasGanadasPc>jugadasGanadasUsuario){
             resultJuego.innerHTML = `
             <div class="eleccU">
-                <h4>📢 El resutado del juego fue: ¡👑Garrita ganó el juego!</h4> 
+                <h4>📢 El resutado del juego es: ¡👑Garrita ganó el juego!</h4> 
             </div>`
     
         }else if(jugadasGanadasPc<jugadasGanadasUsuario){
             resultJuego.innerHTML = `
             <div class="eleccU">
-                <h4>📢 El resutado del juego fue: ¡👑Ganaste el juego ${nombre}!</h4> 
+                <h4>📢 El resutado del juego es: ¡👑Ganaste el juego ${nombre}!</h4> 
             </div>`
     
         }else{
             resultJuego.innerHTML = `
             <div class="eleccU">
-                <h4>📢 El resutado del juego fue: Empate😞</h4> 
+                <h4>📢 El resutado del juego es: Empate😞</h4> 
             </div>`
     
         }
@@ -222,16 +234,20 @@ function determinarGanador(){
         const reiniciar= document.getElementById("reset")
         reiniciar.style.display = 'block';
 
-
-
-    }else if(numeroRondas>5){
         // <--------Deshabilitar botón de elegir----->
         const btnElegir = document.getElementById("mostrar");
         btnElegir.disabled = true;
 
-        // <--------Mostrar el botón de reinicio----->
-        const reiniciar= document.getElementById("reset")
-        reiniciar.style.display = 'block';
+
+
+    // }else if(numeroRondas>5){
+    //     // <--------Deshabilitar botón de elegir----->
+    //     const btnElegir = document.getElementById("mostrar");
+    //     btnElegir.disabled = true;
+
+    //     // <--------Mostrar el botón de reinicio----->
+    //     const reiniciar= document.getElementById("reset")
+    //     reiniciar.style.display = 'block';
 
     }else{
         // <--------Mostrar el botón de reinicio----->
@@ -248,6 +264,7 @@ function determinarGanador(){
     
 
 }
+
 // <-------------Reset Jugada------------------> 
 
 // - también cargar el nombre del Jugador en la tabla de resultados y en un recuadro que informe cómo termina el juego.-decidí no hacer esto porque no logré guardar el nombre en una variable-
